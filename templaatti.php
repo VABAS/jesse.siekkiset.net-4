2a80078f7f14599ff8cfc68596b95105deb91bb0
Jesse Siekkinen <jesse.siekkinen@gmail.com>
2017-01-25 16:41:58 +0200

    Lisätty .gitignore ja templaatti.php

---
+++ templaatti.php
@@ -0,0 +1,7 @@
+<section>
+    <h1>Tämä on templaatti</h1>
+    <p>
+        Templaattitiedostossa ei ole kauheasti sisältöä valmiiksi.
+        Siihen onkin tarkoitus lisätä oma sisältönsä ja tehdä siitä uusi sivu.
+    </p>
+</section>
